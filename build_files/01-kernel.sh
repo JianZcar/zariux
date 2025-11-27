35e283ef2cabf40385cbe8fb3c4f5f9190986f41
JianZcar <esteban.jianzcar@outlook.com>
2025-11-28 02:55:58 +0800

    remove rpmfusion being disable as it doesnt exist

--- build_files/01-kernel.sh
+++ build_files/01-kernel.sh
@@ -46,7 +46,6 @@ mv -f 05-rpmostree.install.bak 05-rpmostree.install
 mv -f 50-dracut.install.bak 50-dracut.install
 popd
 
-dnf5 -y config-manager setopt "*rpmfusion*".enabled=false
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 dnf5 -y install scx-scheds
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
